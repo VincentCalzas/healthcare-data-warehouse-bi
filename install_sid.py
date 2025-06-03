a4e5d27c8ff72c73ba9b929c09f00f90aa700ec1
Aurianne Schwartz <aurianne@example.com>
2025-06-03 12:16:27 +0200

    Ajout de log pour install_sid.py

--- install_sid.py
+++ install_sid.py
@@ -5,6 +5,11 @@ import re
 import dotenv
 
 logger = logging.getLogger(__name__)
+logger.setLevel('INFO')
+logging.basicConfig(    
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO,
+)
 
 # Load environment variables from .env file
 dotenv.load_dotenv()
