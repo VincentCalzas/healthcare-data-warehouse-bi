6b2e2a9f893a27c4dc0475161c9dffa63dfbda79
Camille Milon <camille.milon@tuhh.de>
2025-06-10 10:37:39 +0200

    Merge

--- launch_load_sid.py
+++ launch_load_sid.py
@@ -54,33 +54,19 @@ def connect_snowflake():
     return snowflake.connector.connect(**SNOWFLAKE_CONFIG)
 
 def normalize_timestamp(ts_str):
-
     # Exemple attendu : '2024-04-29-17-25-30'
-
     try:
-
         if ts_str is None:
-
             return None
-
         ts_str = ts_str.replace("#", "0")
-
         parts = ts_str.split('-')
-
         if len(parts) == 6:
-
             date_part = '-'.join(parts[0:3])   # '2024-04-29'
-
             time_part = ':'.join(parts[3:6])   # '17:25:30'
-
             return f"{date_part} {time_part}"
-
         else:
-
             return ts_str
-
     except Exception:
-
         return ts_str
 
 
